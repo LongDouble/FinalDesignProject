5492889aa6498ee7e37cb92ef18da72999a1615b
Austin Goergen <goergena@oregonstate.edu>
2020-05-20 09:33:28 -0700

    Removing unneeded comparator

--- Comparator.sv
+++
@@ -1,6 +0,0 @@
-module comparator #(parameter N = 6, M = 60)(input  logic [N-1:0] a, 
-                                             output logic lt); 
-      
-    assign lt = (a < M); 
-    
-endmodule
