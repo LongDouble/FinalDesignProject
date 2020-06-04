39a3170a83e5a1204ed27edc8a26d3990febef27
BobbySinclusto <benjamal@oregonstate.edu>
2020-06-04 16:52:55 -0700

    added comments

--- IRDriver/ClockDivider.sv
+++ IRDriver/ClockDivider.sv
@@ -1,6 +1,14 @@
+/*
+This module takes parameters for the amount to divide
+the input clock signal by and the number of bits it
+takes to represent that value. It divides the clock
+signal by the specified value and outputs the divided
+clock signal.
+*/
+
 module ClockDivider #(parameter divide_by = 2, N = 2) (input logic clk, reset, output logic clk_div);
     logic [N-1:0] count;
-
+    // Double edge triggered so that we can divide by odd numbers
     always_ff @(posedge clk, negedge clk, posedge reset)
         if (reset)
         begin
@@ -13,7 +21,7 @@ module ClockDivider #(parameter divide_by = 2, N = 2) (input logic clk, reset, o
                 count <= count + 1;
             else
             begin
-                clk_div <= ~clk_div;
+                clk_div <= ~clk_div; // toggle the  output
                 count <= 0;
             end
         end
